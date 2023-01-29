cda7964226b314fbe8e0af9d23b5ac1c3206ba01
roman-supercode <roman.supercode@gmail.com>
2023-01-30 00:35:12 +0100

    frontend & server with MongoDB

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./db/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+
+dotenv.config();
+
+const app = express();
+//middleware
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", async (req, res) => {
+    res.send("Hello from the Server!");
+});
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGO_URL);
+        app.listen(9898, () => console.log("Server ist listening on PORT: ", 9898));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+startServer();
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,46 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+// https://beta.openai.com/docs/libraries/node-js-library
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+// eine eine Instanz der OpenAIApi
+const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+    res.send("Hello from DALLE");
+});
+
+router.route("/").post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        // Anfrage an die OpenAI-API
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64_json",
+        });
+
+        // Antwort (Bild) auslesen
+        const image = aiResponse.data.data[0].b64_json;
+
+        // Bild wird an den Client zurückgegeben
+        res.status(200).json({ photo: image });
+
+    } catch (error) {
+        console.error(error);
+        // Fehlermeldung wird an den Client gesendet
+        res.status(500).send(error?.response.data.error.message);
+    }
+});
+
+export default router;
