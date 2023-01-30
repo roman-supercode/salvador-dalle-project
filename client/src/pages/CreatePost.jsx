4598e8a9e1c8f3772041dcf54d1b383b67044d32
roman-supercode <roman.supercode@gmail.com>
2023-01-30 14:59:20 +0100

    cloudinary setup & search fuction

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,13 +17,33 @@ const CreatePost = () => {
 	const [generatingImg, setGeneratingImg] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-	const handleSubmit = () => {
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
+		if (form.prompt && form.photo) {
+			setLoading(true);
+
+			try {
+				const response = await fetch("http://localhost:9898/api/v1/post", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({ ...form })
+				});
+				await response.json();
+				navigate("/");
+			} catch (error) {
+				alert(error);
+			} finally {
+				setLoading(false);
+			}
+		} else {
+			alert("Please enter a prompt");
+		}
 	};
 
-	const handleChange = (e) => {
-		setForm({ ...form, [e.target.name]: e.target.value });
-	};
+	const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
 	const handleSurpriseMe = () => {
 		const randomPrompt = getRandomPrompt(form.prompt);
@@ -39,11 +59,12 @@ const CreatePost = () => {
 					headers: {
 						"Content-Type": "application/json"
 					},
-					body: JSON.stringify({ prompt: form.prompt })
+					body: JSON.stringify({
+						prompt: form.prompt,
+					})
 				});
 
 				const data = await response.json();
-
 				setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
 			} catch (error) {
 				alert(error);
